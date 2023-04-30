b4f4567731d84eae571343eef5871859849ccd38
Benito Luongo <beluonve@alumni.uv.es>
2023-04-30 16:22:53 +0200

    finally was able to add google maps to flutter, needed maps for android (and add billing :/

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'generatorPage.dart';
-import 'favoritePage.dart';
 import 'homePage.dart';
 
 void main() {
