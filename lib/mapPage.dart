0231594c442683a7ab0684ba0e306dc661ca510f
Benito Luongo <beluonve@alumni.uv.es>
2023-04-30 17:36:21 +0200

    added de markers following the google-maps-in-flutter codelabs v2

--- lib/mapPage.dart
+++ lib/mapPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:provider/provider.dart';
+import 'src/locations.dart' as locations;
 
 import 'main.dart';
 
@@ -10,12 +10,28 @@ class MyMapPage extends StatefulWidget {
 }
 
 class _MyMapPageState extends State<MyMapPage> {
-  late GoogleMapController mapController;
+  // late GoogleMapController mapController;
+  final Map<String, Marker> _markers = {};
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  // final LatLng _center = const LatLng(45.521563, -122.677433);
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    // mapController = controller;
+    final googleOffices = await locations.getGoogleOffices();
+    setState(() {
+      _markers.clear();
+      for (final office in googleOffices.offices) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
+    });
   }
 
   @override
@@ -32,10 +48,11 @@ class _MyMapPageState extends State<MyMapPage> {
         ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(0, 0),
+            zoom: 2.0,
           ),
+          markers: _markers.values.toSet(),
         ),
       ),
     );
